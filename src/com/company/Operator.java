e7904f287f8e408385f0e474e9804f108bb32a45
FexLaboratory <FexLaboratory@gmail.com>
2016-03-10 18:53:52 +0200

    Operators added

---
+++ src/com/company/Operator.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by mfesenko on 3/10/2016.
+ */
+public class Operator {
+    public static void main(String[] args) {
+        int k = 5;
+        System.out.println(k);
+    }
+}
